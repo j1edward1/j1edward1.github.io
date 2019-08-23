908f5b6930148c780da7b17a95f715e5462612eb
Joel Davenport <j1edward1@yahoo.com>
2019-08-23 00:28:30 -0500

    update

--- FPM/shootfinder/1.php
+++ FPM/shootfinder/1.php
@@ -1,25 +1,29 @@
 <?php
-	$mes = "<table><th>Field</th><th>User Input</th>";
-	foreach ($_POST as $key => $value) {
-		$mes = $mes . "<tr><td> ".htmlspecialchars($key)." </td><td> ".htmlspecialchars($value)."</td></tr>";
+	if(isset($_POST['submit'])){
+		$mes = "<table><th>Field</th><th>User Input</th>";
+		foreach ($_POST as $key => $value) {
+			$mes = $mes . "<tr><td> ".htmlspecialchars($key)." </td><td> ".htmlspecialchars($value)."</td></tr>";
+		}
+		$mes = $mes . "</table>";
+	
+	//||||||||||||||||||||||||||
+	
+		$to = "joelfullpackagemedia@gmail.com";
+		$subject = "Shoot Finder Feedback";
+		$message = $mes;
+
+		// Always set content-type when sending HTML email
+		$headers = "MIME-Version: 1.0" . "\r\n";
+		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+		$headers .= 'From: <joelfullpackagemedia@gmail.com>' . "\r\n";
+	
+
+		mail($to,$subject,$message,$headers);
+	//	echo $mes;
 	}
-	$mes = $mes . "</table>";
-	
-//||||||||||||||||||||||||||
-	
-	$to = "joelfullpackmedia@gmail.com";
-	$subject = "Shoot Finder Feedback";
-	$message = $mes;
-
-	// Always set content-type when sending HTML email
-	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-	$headers .= 'From: <joelfullpackagemedia@gmail.com>' . "\r\n";
-	
-
-	mail($to,$subject,$message,$headers);
-//	echo $mes;
-	
+	else {
+		header("Location: https://fpm-shootfinder.tumblr.com");
+	}
 ///////////////////////////////////////////////////////////////////			
 ?>
 <html>
@@ -28,7 +32,7 @@
 <script>
 	$(document).ready(function(){
 	
-		setTimeout(function(){location.assign("https://fpm-shootfinder.tumblr.com");},5);
+		setTimeout(function(){location.assign("https://fpm-shootfinder.tumblr.com");},3000);
 	
 	});
 </script>
